4966e8bb00f66bd7f1f3826502f727016a3c53bc
Kamesh Mahendrawada <kameshsaim@gmail.com>
2019-04-24 12:14:46 +0530

    urls

---
+++ AnalysisResults/AnalysisResults/urls.py
@@ -0,0 +1,31 @@
+"""
+Definition of urls for AnalysisResults.
+"""
+
+from datetime import datetime
+from django.conf.urls import url
+import django.contrib.auth.views
+
+import app.forms
+import app.views
+
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    url(r'^$', app.views.home, name='home'),
+    url(r'^contact$', app.views.contact, name='contact'),
+    url(r'^about$', app.views.about, name='about'),
+    url(r'^party$', app.views.partywise, name='partywise'),
+    url(r'^trend$', app.views.trend, name='trend'),
+    url(r'^const$', app.views.const, name='const'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
